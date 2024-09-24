54faca0d34cfccee85f8798eee2baaa67f71d609
Rifki Rahardjo <rifki.rahardjo@muf.co.id>
2024-09-24 17:53:08 +0700

    Initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
-import "@/styles/globals.css";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from '../redux/store';
+import 'antd/dist/reset.css'; // Import Ant Design styles
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+import AppLayout from '../components/Layout'; // Import Layout yang baru
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Provider store={store}>
+      <AppLayout>
+        <Component {...pageProps} />
+        </AppLayout>
+      </Provider>
+    </>
+  );
 }
+
+export default MyApp;
